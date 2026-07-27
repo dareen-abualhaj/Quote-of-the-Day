d019e6f465f21396dbdce2e5c14d3a52fa68abd2
Saja Sayara <sajaahmadsayara4@gmail.com>
2026-07-27 21:38:55 +0300

    feat: add list_categories input schema

---
+++ src/schemas/list-categories.ts
@@ -0,0 +1,7 @@
+import * as z from "zod/v4";
+
+/**
+ * Schema for tool: list_categories
+ * Description: Fetches all available categories/tags for quote filtering.
+ */
+export const ListCategoriesInputSchema = z.object({});
