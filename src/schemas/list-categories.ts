884fd58ec6d93fb5ae61d153b39092a5212e4742
Saja Sayara <sajaahmadsayara4@gmail.com>
2026-08-12 21:23:12 +0300

    Update list-categories.ts

--- src/schemas/list-categories.ts
+++ src/schemas/list-categories.ts
@@ -1,4 +1,10 @@
 // schemas/list-categories.ts
 import * as z from "zod/v4";
-
-export const ListCategoriesInputSchema = {};
+export const ListCategoriesInputSchema = {
+  limit: z
+    .number()
+    .int()
+    .min(1, "Limit must be at least 1.")
+    .max(50, "Limit cannot exceed 50.")
+    .optional(),
+};
