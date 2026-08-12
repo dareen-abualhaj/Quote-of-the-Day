baa8ece9cf5642fc17d035d17a24ce5a66d8c128
Saja Sayara <sajaahmadsayara4@gmail.com>
2026-08-12 21:53:17 +0300

    Update list-categories.ts

--- src/schemas/list-categories.ts
+++ src/schemas/list-categories.ts
@@ -4,7 +4,5 @@ export const ListCategoriesInputSchema = {
   limit: z
     .number()
     .int()
-    .min(1, "Limit must be at least 1.")
-    .max(50, "Limit cannot exceed 50.")
     .optional(),
 };
